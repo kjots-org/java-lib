ed1eccd44d778e14a52760c05ca2f27d7ff1008c
kjots@kjots.org <kjots@kjots.org>
2011-03-23 12:06:17 +1100

    Added java.net.URI emulation to kjots.org GWT Common module.

--- gwt/common/src/test/java/org/kjots/lib/common/gwt/client/emul/java/net/URIGwtTest.java
+++ gwt/common/src/test/java/org/kjots/lib/common/gwt/client/emul/java/net/URIGwtTest.java
@@ -17,9 +17,34 @@ import org.kjots.lib.common.gwt.client.CommonGwtTestBase;
  */
 public class URIGwtTest extends CommonGwtTestBase {
   /**
-   * Test the creation of a URI.
+   * Test the creation of a generic URI.
    */
-  public void testCreateUri() {
-    assertNotNull(URI.create("scheme:/path"));
+  public void testCreateGenericUri() {
+    URI genericUri = URI.create("scheme:scheme-specific-part#fragment");
+    
+    assertNotNull(genericUri);
+    
+    assertEquals("scheme", genericUri.getScheme());
+    assertEquals("scheme-specific-part", genericUri.getSchemeSpecificPart());
+    assertEquals("fragment", genericUri.getFragment());
+  }
+  
+  /**
+   * Test the creation of a hierarchical URI.
+   */
+  public void testCreateHierarchicalUri() {
+    URI hierarchicalUri = URI.create("scheme://user-info@host.name:1234/path?query#fragment");
+    
+    assertNotNull(hierarchicalUri);
+    
+    assertEquals("scheme", hierarchicalUri.getScheme());
+    assertEquals("//user-info@host.name:1234/path?query", hierarchicalUri.getSchemeSpecificPart());
+    assertEquals("user-info@host.name:1234", hierarchicalUri.getAuthority());
+    assertEquals("user-info", hierarchicalUri.getUserInfo());
+    assertEquals("host.name", hierarchicalUri.getHost());
+    assertEquals(1234, hierarchicalUri.getPort());
+    assertEquals("/path", hierarchicalUri.getPath());
+    assertEquals("query", hierarchicalUri.getQuery());
+    assertEquals("fragment", hierarchicalUri.getFragment());
   }
 }
