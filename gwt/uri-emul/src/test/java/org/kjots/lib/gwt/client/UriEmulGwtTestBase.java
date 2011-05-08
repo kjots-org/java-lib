4399d028811779445e164282a99e6734f8d3d852
kjots@kjots.org <kjots@kjots.org>
2011-05-08 12:44:10 +1000

    Moved URI emulation code from GWT common module to GWT URI emulation module and removed GWT common module.

---
+++ gwt/uri-emul/src/test/java/org/kjots/lib/gwt/client/UriEmulGwtTestBase.java
@@ -0,0 +1,26 @@
+/* 
+ * Copyright © 2011 Karl J. Ots.  All Rights Reserved.
+ */
+package org.kjots.lib.gwt.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * java.net.URI Emulation GWT Test Base.
+ * <p>
+ * Created: 23rd March 2011.
+ *
+ * @author <a href="mailto:kjots@kjots.org">Karl J. Ots &lt;kjots@kjots.org&gt;</a>
+ * @since 99-SNAPSHOT
+ */
+public abstract class UriEmulGwtTestBase extends GWTTestCase {
+  /**
+   * Retrieve the name of the GWT module.
+   *
+   * @return The name of the GWT module.
+   */
+  @Override
+  public final String getModuleName() {
+    return "org.kjots.lib.UriEmulTest";
+  }
+}
---
+++ gwt/uri-emul/src/test/java/org/kjots/lib/gwt/test/UriEmulGwtTestSuite.java
@@ -0,0 +1,55 @@
+/* 
+ * Copyright © 2011 Karl J. Ots.  All Rights Reserved.
+ */
+package org.kjots.lib.gwt.test;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.kjots.lib.gwt.client.org.apache.harmony.luni.tests.java.net.URIGwtTest;
+import org.kjots.lib.gwt.client.org.apache.harmony.luni.tests.java.net.URISyntaxExceptionGwtTest;
+
+/**
+ * java.net.URI Emulation GWT Test Suite.
+ * <p>
+ * Created: 23rd March 2011.
+ *
+ * @author <a href="mailto:kjots@kjots.org">Karl J. Ots &lt;kjots@kjots.org&gt;</a>
+ * @since 99-SNAPSHOT
+ */
+public class UriEmulGwtTestSuite extends GWTTestSuite {
+  /**
+   * Create the test suite.
+   *
+   * @return The test suite.
+   */
+  public static Test suite() {
+    UriEmulGwtTestSuite suite = new UriEmulGwtTestSuite();
+    
+    addTestSuites(suite);
+    
+    return suite;
+  }
+  
+  /**
+   * Add the test suites.
+   *
+   * @param suite The test suite.
+   */
+  public static void addTestSuites(TestSuite suite) {
+    suite.addTestSuite(URIGwtTest.class);
+    suite.addTestSuite(URISyntaxExceptionGwtTest.class);
+  }
+  
+  /**
+   * Construct a new java.net.URI Emulation GWT Test Suite.
+   * <p>
+   * This constructor is declared <code>private</code> to prevent external
+   * instantiation.
+   */
+  private UriEmulGwtTestSuite() {
+    super("java.net.URI Emulation GWT Test Suite.");
+  }
+}
